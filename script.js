0a4ad73c24502a47b3793a0bc411a71745700ec1
Tiny <tinyspirit99@gmail.com>
2021-11-11 13:49:28 +0700

    baitap2

---
+++ script.js
@@ -0,0 +1,28 @@
+function selectValue() {
+    const selectValue = document.getElementById("select").value;
+    const city = document.getElementsByClassName("city");
+    const odd = document.getElementById("odd");
+    const even = document.getElementById("even");
+
+    console.log(selectValue);
+    for (let i = 1; i <= city.length; i++) {
+        if (selectValue != i) {
+            city[i - 1].style.backgroundColor = "white";
+        } else {
+            city[i - 1].style.backgroundColor = "yellow";
+        }
+    }
+    if (selectValue == "odd") {
+        for (let i = 1; i <= city.length; i++) {
+            if ((i - 1) % 2 == 0) {
+                city[i - 1].style.backgroundColor = "yellow";
+            }
+        }
+    } else if (selectValue == "even") {
+        for (let i = 1; i <= city.length; i++) {
+            if ((i) % 2 == 0) {
+                city[i - 1].style.backgroundColor = "yellow";
+            }
+        }
+    }
+}
