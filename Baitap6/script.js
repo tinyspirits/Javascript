3cfafa83e659a711ce0ce1379fc478660629c8ba
Tiny <tinyspirit99@gmail.com>
2021-11-15 17:22:17 +0700

    baitap6

--- Baitap6/script.js
+++ Baitap6/script.js
@@ -1,7 +1,6 @@
 function selectAll() {
     const check = document.getElementById("selectAllRow");
     const checkValue = document.getElementsByClassName("checkboxInput");
-
     for (let i = 0; i < checkValue.length; i++) {
         if (check.checked == true) {
             checkValue[i].checked = true;
@@ -12,12 +11,10 @@ function selectAll() {
 }
 
 //function Add
-
 function addRow() {
     const titleRow = document.getElementsByClassName("titleRow");
     const createRow = document.createElement("TR");
     const createRowTd = document.createElement("TD");
-
     for (let i = 0; i <= titleRow.length; i++) {
         if (i == 0) {
             let inputTag = document.createElement("INPUT");
@@ -28,20 +25,16 @@ function addRow() {
         } else {
             let createRowTd = document.createElement("TD");
             createRow.appendChild(createRowTd);
-
             createRowTd.addEventListener("dblclick", Edit);
             createRowTd.addEventListener('focusout', (event) => {
                 createRowTd.contentEditable = false;
             });
-
             if (i == titleRow.length) {
                 let createButton = document.createElement("input");
                 createButton.type = "submit";
                 createButton.setAttribute("class", "deleteButton");
-
                 createButton.onclick = function () {
                     const deleteButton = document.getElementsByClassName("deleteButton");
-
                     for (let i = 0; i < deleteButton.length; i++) {
                         createRow.setAttribute("id", ("delete" + i));
                         document.getElementById("delete" + i).remove();
@@ -67,7 +60,6 @@ function addRow() {
 //function Delete
 function deleteRow() {
     const checkboxInput = document.getElementsByClassName("checkboxInput");
-
     for (let i = 0; i < checkboxInput.length; ++i) {
         if (checkboxInput[i].checked == true) {
             document.getElementById("tableData").deleteRow(i + 1);
