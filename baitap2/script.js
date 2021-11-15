ee79b95f582d42249db6d07ee88353ea1602d2c7
Tiny <tinyspirit99@gmail.com>
2021-11-15 19:36:39 +0700

    fix bug

--- baitap2/script.js
+++ baitap2/script.js
@@ -1,24 +1,24 @@
 function selectValue() {
-    const selectValue = document.getElementById("select").value;
+    const select = document.getElementById("select").value;
     const city = document.getElementsByClassName("city");
     const odd = document.getElementById("odd");
     const even = document.getElementById("even");
 
     for (let i = 1; i <= city.length; i++) {
-        if (selectValue != i) {
+        if (select != i) {
             city[i - 1].style.backgroundColor = "white";
         } else {
             city[i - 1].style.backgroundColor = "yellow";
         }
-    }
-    if (selectValue == "odd") {
-        for (let i = 1; i <= city.length; i++) {
+
+        if (select == "odd") {
+
             if ((i - 1) % 2 == 0) {
                 city[i - 1].style.backgroundColor = "yellow";
             }
-        }
-    } else if (selectValue == "even") {
-        for (let i = 1; i <= city.length; i++) {
+
+        } else if (select == "even") {
+
             if ((i - 1) % 2 != 0) {
                 city[i - 1].style.backgroundColor = "yellow";
             }
