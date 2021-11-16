17a03791b3b5f4d55c7cfe6360a23f182661b06f
Tiny <tinyspirit99@gmail.com>
2021-11-16 18:16:30 +0700

    validate

--- Baitap6/script.js
+++ Baitap6/script.js
@@ -1,6 +1,7 @@
 function selectAll() {
     const check = document.getElementById("selectAllRow");
     const checkValue = document.getElementsByClassName("checkboxInput");
+
     for (let i = 0; i < checkValue.length; i++) {
         if (check.checked == true) {
             checkValue[i].checked = true;
@@ -10,12 +11,25 @@ function selectAll() {
     }
 }
 
+//check validate
+const nameRegex = /([A-Za-z0-9_]|[<br>])$/gi;
+const phoneRegex = /(0[3|5|7|8|9])+([0-9]{8})\b/;
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})|[<br>])$/;
+
+function checkValidate(regex, text) {
+    if (regex.test(text) == true) {
+        return true;
+    }
+    return false;
+}
+
 //function Add
 function addRow() {
     const titleRow = document.getElementsByClassName("titleRow");
     const createRow = document.createElement("TR");
     const createRowTd = document.createElement("TD");
-    for (let i = 0; i <= titleRow.length; i++) {
+
+    for (let i = 0; i <= titleRow.length; i++) { // lấy tới cuối mảng
         if (i == 0) {
             let inputTag = document.createElement("INPUT");
             createRow.appendChild(createRowTd);
@@ -24,17 +38,40 @@ function addRow() {
             inputTag.type = "checkbox";
         } else {
             let createRowTd = document.createElement("TD");
+            createRowTd.innerHTML = "";
             createRow.appendChild(createRowTd);
-            createRowTd.addEventListener("dblclick", edit);
+            console.log(titleRow[i - 1].innerHTML);
+            createRowTd.addEventListener("dblclick", Edit);
             createRowTd.addEventListener('focusout', (event) => {
+                if (titleRow[i - 1].innerHTML == "Name") {
+                    if (checkValidate(nameRegex, createRowTd.innerHTML) == false) {
+                        alert("Chưa nhập đúng định dạng");
+                        createRowTd.innerHTML = "";
+                    }
+                }
+                if ((titleRow[i - 1].innerHTML == "Phone")) {
+                    if (checkValidate(phoneRegex, createRowTd.innerHTML) == false) {
+                        alert("Chưa nhập đúng định dạng");
+                        createRowTd.innerHTML = "";
+                    }
+                }
+                if ((titleRow[i - 1].innerHTML == "Email")) {
+                    if (checkValidate(emailRegex, createRowTd.innerHTML) == false) {
+                        alert("Chưa nhập đúng định dạng");
+                        createRowTd.innerHTML = "";
+                    }
+                }
                 createRowTd.contentEditable = false;
             });
+
             if (i == titleRow.length) {
                 let createButton = document.createElement("input");
                 createButton.type = "submit";
                 createButton.setAttribute("class", "deleteButton");
+
                 createButton.onclick = function () {
                     const deleteButton = document.getElementsByClassName("deleteButton");
+
                     for (let i = 0; i < deleteButton.length; i++) {
                         createRow.setAttribute("id", ("delete" + i));
                         document.getElementById("delete" + i).remove();
@@ -44,7 +81,7 @@ function addRow() {
                 createButton.value = "Delete";
                 createRowTd.appendChild(createButton);
             }
-            function edit() {
+            function Edit() {
                 if (i == titleRow.length) {
                     createRowTd.contentEditable = false;
                 } else {
@@ -52,7 +89,6 @@ function addRow() {
                 }
             }
         }
-
     }
     document.getElementById("tableData").appendChild(createRow);
 }
@@ -60,6 +96,7 @@ function addRow() {
 //function Delete
 function deleteRow() {
     const checkboxInput = document.getElementsByClassName("checkboxInput");
+
     for (let i = 0; i < checkboxInput.length; ++i) {
         if (checkboxInput[i].checked == true) {
             document.getElementById("tableData").deleteRow(i + 1);
